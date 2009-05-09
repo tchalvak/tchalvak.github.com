ad9b16abfc8df3529d1db9dd6b542960673197cb
Roy Ronalds <tchalvak@ninjawars.net>
2009-05-09 03:43:03 -0400

    Images: Added all the old images.

---
+++ index2.php
@@ -0,0 +1,87 @@
+<?php //Php location start echo "PHP active \n";?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+    <meta name="verify-v1" content="ovEKFPSIqgrkomjOmbIyhLTQzUxP4XR4jA+3Gw+WkUg=" />
+  <title> Tangent Parados</title>
+  <link rel="stylesheet" href="./e/css/css.css" type="text/css"></link>
+  <!-- THIS PAGE WITHIN NXS.VZE.COM PARAMETERS.  -->  <!-- Best viewed with: IE, CSS, and westminster font. -->
+  <script language="javascript" type="text/javascript">
+  <!--
+  //function sf(){document.f.q.focus();}
+  //var address=
+  //function loadwindow()
+  //{window.open("http://nxs.vze.com","","width=800,height=500,status=1")}
+  -->
+  </script>
+  <style type="text/css"><!-- a {text-decoration:none;font-weight:bold}  --> </style>
+  <!--
+  //To add to script: function setfocus() {document.formname.elementname.focus();}
+  //To add to body: onLoad="setfocus(no idea what goes here)" 
+  -->
+  </head>
+
+<body text="#cccccc" link="#00cc00" vlink="#999999" alink="#ff0000" bgcolor="#000000" background="./i/creations/digitalcentipede.jpg">
+  <div align="center" class="subtitle"> 
+    Reality Biproducts, Version 5.9.1:<!-- Page Update TEST, also get a counter that will input here. -->
+    <br />
+    <u>Thinking about things, so you don't have to.</u><br /><p></p>
+  </div>
+
+  <div align="left"><!-- LEFT-->
+    <a href="#gmail" onclick="window.open('http://gmail.google.com/gmail','Gmail','scrollbars,resizable,location');"> Gmail </a>-
+    <a href="#dicionary" onclick="window.open('http://www.thesaurus.com','ThesDictionary','scrollbars,resizable,location');">Dicthesaurus</a>-    <a href="#wikipedia" onclick="window.open('http://en.wikipedia.org/wiki/Main_Page','Wikipedia','scrollbars,resizable,location');">Wikipedia</a>
+    <br />
+    <a href="#hotmail" onclick="window.open('http://www.hotmail.com','Hotmail','scrollbars,resizable,location,menubar=yes,status=3');">Hotmail</a>-<a href="#webmail" onclick="window.open('http://webmail.geneseo.edu','Webmail','scrollbars,resizable,location,menubar=yes,status=7');">Webmail</a>-<a href="http://www.geneseo.edu/~ror1/AGmud.exe">MUD</a>-<a href="#connector" onclick="window.open('http://www.mudconnector.com','Tangent','scrollbars,resizable,location');">Connector</a><br />
+    <a href="#girlgenius" onclick="window.open('http://www.girlgeniusonline.com','Tangent','scrollbars,resizable,location,status=5');">GirlGenius</a>-<a href="#schlock" onclick="window.open('http://www.schlockmercenary.com','Tangent','scrollbars,resizable,location,status=5');">Schlock</a>-<a href="#sluggyfreelance" onclick="window.open('http://sluggy.com','Tangent','scrollbars,resizable,location,status=5');">Sluggy</a>-<a href="#crfh" onclick="window.open('http://www.crfh.net/','Tangent','scrollbars,resizable,location,status=5');">Crfh</a><br />
+    <a href="#Ninjawars" onclick="window.open('http://www.ninjawars.net/webgame/','Ninjas','scrollbars,resizable,location,status=8');">Ninja</a>-<a href="#Ninjawarsforum" onclick="window.open('http://www.ninjawars.net/forum/','Forum','scrollbars,resizable,location,status=8');">Forum</a>-<a href="#xaos" onclick="window.open('http://www.xaos-mud.org/forum/','Xaos','scrollbars,resizable,location');">Xaos</a>-<a href="#gagg" onclick="window.open('http://gagg.geneseo.edu/phpBB2/','Gagg','scrollbars,resizable,location');">Gagg</a>-<a href="#reading" onclick="window.open('http://www.pagebypagebooks.com','Reading','scrollbars,resizable,location,status=8');">Reading</a>
+    <form method="get" action="http://www.google.com/search" name="searchbox">
+<!-- Must find autofocus code for cursor. -->
+<input type="text" style="background-color:black;color:#00cc00;font-weight:bold;" name="q" size="25" maxlength="255" value="" />
+</form>
+</div>
+
+<div align="center"><!-- CORE-->
+<div class="title">
+<img src="./i/creations/crystalshattering.gif" alt="" />
+</div>
+<br />
+<div class="orange">Shards of:</div>
+<a href="#light" onclick="window.open('./i/i.html','Images','scrollbars,resizable,location,status=2');">Light</a>-
+<a href="#stories" onclick="window.open('./n/n.html','Text','scrollbars,resizable,location,status=1');">Text</a>-
+<a href="#action" onclick="window.open('./e/e.html','Code','scrollbars,resizable,location,status=2');">Code</a>-
+<a href="#internet" onclick="window.open('./x/x.html','Shard','scrollbars,resizable,location,status=3');">Internet</a>-
+<a href="http://tchalvak.pbwiki.com" target="_blank" >Wiki</a><br />
+</div>
+
+
+<div align="right"><!-- RIGHT-->
+<a href="#artworkdir" onclick="window.open('./i/creations/','Images','scrollbars,resizable,location,status=2');">Artwork /Dir</a><br />
+<a href="#deviantart" onclick="window.open('http://dnaexmosn.deviantart.com/','DeviantArt','scrollbars,resizable,location');">Deviant Art Gallery</a><br />
+<!-- Remember to change the link href, the link text, the image source, and the image dimensions. -->
+<a href="#latestimage" id="latestimage" onclick="window.open('./i/creations/manyeyessmall.gif','Images','scrollbars,resizable,location,status=2');">
+Many Eyes, One World<br />
+<img style="border:0" src="./i/creations/manyeyessmall.gif" width="50" height="50" alt="Many eyes" /> <br />
+</a>
+<a class="fade" href="mailto:tchalvak@gmail.com">tchalvak@</a><br />
+<b>AIM: Tchalvak</b><br/>
+<a href="#conceptart" onclick="window.open('http://conceptart.org/forums/','Conceptart','scrollbars,resizable,location');">Conceptart.org</a>
+</div>
+
+<div class="fade" align="center"><!-- BOTTOM-->
+
+<!-- Remember to change the link href, the latest date, and the image source. -->
+<a class="fade" href="#latestimage" 
+onclick="window.open('./i/entities/unknownalienbugs.jpg','Images','scrollbars,resizable,location,status=2');">
+Latest Mirrored Image: July 13th, 2005</a><br />
+<!-- Image hidden for now <img src="./i/entities/unknownalienbugs.jpg" alt="mirrored image" /><br /> -->
+
+<a class="fade" href="#mirrorimagedir" onclick="window.open('./i/entities/','Images','scrollbars,resizable,location,status=2');">Mirror /Dir</a><br />
+<a href="http://www.shorturl.com/ref/in.cgi?nxs.vze.com" target="_blank" class="fade">ShortURL.com</a><br />
+<a href="http://validator.w3.org/check?uri=referer" target="_blank" class="fade">Check Page Code</a><br />
+</div>
+
+</body>
+
+</html>
