3e6e7d17136efdb70197b0074171c85ce0dd7842
Roy Ronalds <tchalvak@ninjawars.net>
2009-08-12 23:33:11 -0400

    Index: minor change.

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
       <u>Thinking about things, so you don't have to.</u><br /><p></p>
     </div>
 
-    <div style="width:33%;text-align:left:position:relative;float:left;background-image:url('orange-storm-corner.jpg');background-repeat:no-repeat;" class="left"><!-- LEFT-->
+    <div style="width:33%;text-align:left:position:relative;float:left;background-image:url('./i/orange-storm-corner.jpg');background-repeat:no-repeat;" class="left"><!-- LEFT-->
     <a href="#gmail" onclick="window.open('http://gmail.google.com/gmail','Gmail','');"> Gmail </a>-<a href="#calender" onclick="window.open('http://www.google.com/calendar/render?pli=1','Calendar','');">Calendar</a>-<a href="#dicionary" onclick="window.open('http://www.thesaurus.com','ThesDictionary','');">Dicthesaurus</a>-    <a href="#wikipedia" onclick="window.open('http://en.wikipedia.org/wiki/Main_Page','Wikipedia','');">Wikipedia</a>-<a href="#music" onclick="window.open('http://www.playlist.com', 'Music', '');">Music</a>
       <br />
     <a target='_new' href='http://www.elfquest.com/gallery/OnlineComics3.html'>Elfquest</a>-<a href="#reading" onclick="window.open('http://www.pagebypagebooks.com', 'Reading', '');">Reading</a>-<a href="http://www.geneseo.edu/~ror1/AGmud.exe">MUD</a>-<a href="#connector" onclick="window.open('http://www.mudconnector.com','Tangent','');">Connector</a>-<a href="#facebook" onclick="window.open('http://www.facebook.com', 'Facebook', '');">Facebook</a>
@@ -40,7 +40,7 @@
       <a href="#Ninjawars" onclick="window.open('http://www.ninjawars.net/webgame/','Ninjas','');">Ninja</a>-<a href="#Ninjawarsforum" onclick="window.open('http://ninjawars.proboards19.com/','Forum','');">Forum</a>-<a href="#xaos" onclick="window.open('http://www.xaos-mud.org/forum/','Xaos','');">Xaos</a>-<a href="#gagg" onclick="window.open('http://gagg.geneseo.edu/phpBB2/','Gagg','');">Gagg</a>-<a href="#nwtest" onclick="window.open('http://ninjawars.net/~tchalvak/ninjawars/trunk/webgame/index.php','NWTEST','');">Nwtest</a>-<a href="#nwgithub" onclick="window.open('http://github.com/tchalvak/ninjawars/tree/master', 'NWGitHub', '');">NwGitHub</a><br />
     </div>
 
-    <div style="width:33%;text-align:center;position:relative;float:left;background-image:url('orange-storm-corner.jpg');background-repeat:no-repeat;" class="core"><!-- CORE-->
+    <div style="width:33%;text-align:center;position:relative;float:left;background-image:url('./i/orange-storm-corner.jpg');background-repeat:no-repeat;" class="core"><!-- CORE-->
       <div class="title">
         <img src="./i/creations/crystalshattering.gif" alt="" />
       </div>
@@ -56,7 +56,7 @@
     </div>
 
 
-    <div style="position:relative;width:33%;text-align:right;float:left;background-image:url('orange-storm-corner.jpg');background-repeat:no-repeat;" class="right"><!-- RIGHT-->
+    <div style="position:relative;width:33%;text-align:right;float:left;background-image:url('./i/orange-storm-corner.jpg');background-repeat:no-repeat;" class="right"><!-- RIGHT-->
       <a href="#artworkdir" onclick="window.open('./i/creations/','Images','status=2');">Artwork /Dir</a><br />
       <a href="#deviantart" onclick="window.open('http://dnaexmosn.deviantart.com/','DeviantArt','');">Deviant Art Gallery</a><br />
       <!-- Remember to change the link href, the link text, the image source, and the image dimensions. -->
