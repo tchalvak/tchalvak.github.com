60baf4b0b1ef1498e67a8facc3416f114b1f3e65
Roy <tchalvakspam@gmail.com>
2009-09-14 17:08:44 -0400

    nxs: Copied .html to .php file.

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <link rel="SHORTCUT ICON" href="./favicon.ico"></link>
     <link rel="stylesheet" href="./e/css/css.css" type="text/css"></link>
     <!-- THIS PAGE WITHIN NXS.VZE.COM PARAMETERS.  -->  <!-- Best viewed with: IE, CSS, and westminster font. -->
+    <script language="javascript" type="text/javascript" src="js/debug.js"></script>
     <script language="javascript" type="text/javascript">
       <!--
       //function sf(){document.f.q.focus();}
@@ -168,13 +169,15 @@
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js" type="text/javascript"></script>
     <script type="text/javascript">
       function repositoryLoad() {
-        var limit = 10        // how many repos to list
+        var limit = 20        // how many repos to list
         var login = 'tchalvak' // your username
 
         $.getJSON('http://github.com/api/v1/json/' + login + '?callback=?', function(data) {
           var repos = $.grep(data.user.repositories, function() {
             return !this.fork
           })
+          
+
 
           repos.sort(function(a, b) {
             return b.watchers - a.watchers
@@ -186,8 +189,36 @@
         })
 
 		$("#waiting").hide(); // Hide the not yet loaded message.
-        
-      }
+		
+		
+      } // End of repositoryLoad function.
+      
+      function loadLastCommitMessage(){
+        var login = 'tchalvak' // your username
+              
+         $.getJSON('http://github.com/api/v1/json/' + login + '/ninjawars/commit/master/?callback=?', function(data) {
+            var unknown = $.grep(data.commit, function() {
+                return true;
+          })
+          /*var_dump(unknown);
+          
+          var_dump(data);
+          var_dump(data.commit);
+          var_dump(data.commit.message);
+          var_dump(data.commit.url);
+          var_dump(data.commit.author.name);
+          */
+          
+              // Load latest commit message.
+			$('#latest-commit').html(data.commit.message);
+			$('#latest-commit').append("<div id='commit-author'>--"+data.commit.author.name+"</div>");
+			$('#latest-commit-title').show();        
+			$('#latest-commit').show();
+          
+          });
+          
+		}        
+      
       
        $(document).ready(function() {
        		$("#repos").append("<div id='waiting'>Repositories not yet loaded.</div>");
@@ -196,9 +227,18 @@
            });
            
            
-			setTimeout("repositoryLoad()", 10000); // Only load the repositories after a delay.
-
            
+			$('#latest-commit').hide();
+			$('#latest-commit-title').hide();
+			           
+			setTimeout("repositoryLoad()", 5000); // Only load the repositories after a delay.
+			
+			//$('#latest-commit').load("index.html .subtitle"); //  #commit .message
+			//$('#j').load('http://github.com/tchalvak/ninjawars/tree/master #commit');
+			//$('#latest-commit').show();
+			//$('#latest-commit-title').show();
+
+           			loadLastCommitMessage();
            
          });
     </script>
@@ -216,16 +256,16 @@
     <div class="left"><!-- LEFT-->
     <ul>
       <li>
-    <a href="#gmail" onclick="window.open('http://gmail.google.com/gmail','Gmail','');"> Gmail </a>-<a href="#calender" onclick="window.open('http://www.google.com/calendar/render?pli=1','Calendar','');">Calendar</a>-<a href="#dicionary" onclick="window.open('http://www.thesaurus.com','ThesDictionary','');">Dicthesaurus</a>-    <a href="#wikipedia" onclick="window.open('http://en.wikipedia.org/wiki/Main_Page','Wikipedia','');">Wikipedia</a>-<a href="#music" onclick="window.open('http://www.playlist.com', 'Music', '');">Music</a>
+    <a target='_blank' href="http://gmail.google.com/gmail"> Gmail </a>-<a target='_blank' href="http://www.google.com/calendar/render?pli=1">Calendar</a>-<a target='_blank' href="http://www.thesaurus.com">Dicthesaurus</a>-    <a target='_blank' href="http://en.wikipedia.org/wiki/Main_Page">Wikipedia</a>-<a target='_blank' href="http://www.playlist.com">Music</a>
       </li>
       <li>
-    <a target='_new' href='http://www.elfquest.com/gallery/OnlineComics3.html'>Elfquest</a>-<a href="#reading" onclick="window.open('http://www.pagebypagebooks.com', 'Reading', '');">Reading</a>-<a href="http://www.geneseo.edu/~ror1/AGmud.exe">MUD</a>-<a href="#connector" onclick="window.open('http://www.mudconnector.com','Tangent','');">Connector</a>-<a href="#facebook" onclick="window.open('http://www.facebook.com', 'Facebook', '');">Facebook</a>
+    <a target='_blank' href='http://www.elfquest.com/gallery/OnlineComics3.html'>Elfquest</a>-<a target='_blank' href="http://www.pagebypagebooks.com">Reading</a>-<a target='_blank' href="http://www.geneseo.edu/~ror1/AGmud.exe">MUD</a>-<a target='_blank' href="http://www.mudconnector.com">Connector</a>-<a target='_blank' href="http://www.facebook.com">Facebook</a>
       </li>
       <li>
-      <a href="#girlgenius" onclick="window.open('http://www.girlgeniusonline.com/comic.php','Tangent','');">GirlGenius</a>-<a href="#schlock" onclick="window.open('http://www.schlockmercenary.com','Tangent','');">Schlock</a>-<a href="#crfh" onclick="window.open('http://www.crfh.net/','Tangent','');">Crfh</a>-<a href="#qwantz" onclick="window.open('http://qwantz.com', 'Tangent','');">Qwantz</a>-<a href="#softer" onclick="window.open('http://www.asofterworld.com/index.php', 'Tangent', '');">Softer</a><br />
+      <a target='_blank' href="http://www.girlgeniusonline.com/comic.php">GirlGenius</a>-<a target='_blank' href="http://www.schlockmercenary.com">Schlock</a>-<a target='_blank' href="http://www.crfh.net/">Crfh</a>-<a target='_blank' href="http://qwantz.com">Qwantz</a>-<a target='_blank' href="http://www.asofterworld.com/index.php">Softer</a><br />
       </li>
       <li>
-      <a href="#Ninjawars" onclick="window.open('http://www.ninjawars.net/webgame/','Ninjas','');">Ninja</a>-<a href="#Ninjawarsforum" onclick="window.open('http://ninjawars.proboards19.com/','Forum','');">Forum</a>-<a href="#gagg" onclick="window.open('http://gagg.geneseo.edu/phpBB2/','Gagg','');">Gagg</a>-<a href="#nwtest" onclick="window.open('http://ninjawars.net/~tchalvak/ninjawars/trunk/webgame/index.php','NWTEST','');">Nwtest</a>-<a href="#nwgithub" onclick="window.open('http://github.com/tchalvak/ninjawars/tree/master', 'NWGitHub', '');">NwGitHub</a><br />
+      <a target='_blank' href="http://www.ninjawars.net/webgame/">Ninja</a>-<a target='_blank' href="http://ninjawars.proboards19.com/">Forum</a>-<a target='_blank' href="http://gagg.geneseo.edu/phpBB2/">Gagg</a>-<a target='_blank' href="http://ninjawars.net/~tchalvak/ninjawars/trunk/webgame/index.php">Nwtest</a>-<a target='_blank' href="http://github.com/tchalvak/ninjawars/tree/master">NwGitHub</a><br />
       </li>
      </ul>
     </div>
@@ -238,41 +278,46 @@
       <div class="orange">
         Shards of:
       </div>
-      <a href="#light" onclick="window.open('./i/i.html','Images','status=2');">Light</a>-
-      <a href="#stories" onclick="window.open('./n/n.html','Text','status=1');">Text</a>-
-      <a href="#action" onclick="window.open('./e/e.html','Code','status=2');">Code</a>-
-      <a href="#internet" onclick="window.open('./x/x.html','Shard','status=3');">Internet</a>-
-      <a href="http://tchalvak.pbwiki.com">Wiki</a><br />
+      <a target='_blank' href="./i/i.html">Light</a>-
+      <a target='_blank' href="./n/n.html');">Text</a>-
+      <a target='_blank' href="./e/e.html">Code</a>-
+      <a target='_blank' href="./x/x.html">Internet</a>-
+      <a target='_blank' href="http://tchalvak.pbwiki.com">Wiki</a><br />
     </div>
 
 
     <div class="right"><!-- RIGHT-->
-      <a href="#artworkdir" onclick="window.open('./i/creations/','Images','status=2');">Artwork /Dir</a><br />
-      <a href="#deviantart" onclick="window.open('http://dnaexmosn.deviantart.com/','DeviantArt','');">Deviant Art Gallery</a><br />
+        <ul>
+      <li><a target='_blank' href="./i/creations/">Artwork /Dir</a></li>
+      <li><a target='_blank' href="http://dnaexmosn.deviantart.com/">Deviant Art Gallery</a></li>
       <!-- Remember to change the link href, the link text, the image source, and the image dimensions. -->
-      <a href="#latestimage" id="latestimage" onclick="window.open('./i/creations/manyeyessmall.gif','Images','status=2');">Many Eyes, One World<br />
-        <img style="border:0" src="./i/creations/manyeyessmall.gif" width="50" height="50" alt="Many eyes" /> <br />
-      </a>
-      <a class="fade" href="mailto:tchalvakSPAM@gmail.com">tchalvak@</a><br />
-      <b>AIM: Tchalvak</b><br/>
-      <a href="#conceptart" onclick="window.open('http://conceptart.org/forums/','Conceptart','');">Conceptart.org</a>
+      <li><a target='_blank' id="latestimage" href="./i/creations/manyeyessmall.gif">Many Eyes, One World
+        <img style="border:0" src="./i/creations/manyeyessmall.gif" width="50" height="50" alt="Many eyes" />
+      </a></li>
+      <li><a class="fade" href="mailto:tchalvakSPAM@gmail.com">tchalvak@</a></li>
+      <li><b>AIM: Tchalvak</b></li>
+      <li><a target='_blank' href="http://conceptart.org/forums/">Conceptart.org</a></li>
     </div>
     
     
 
     <div class="bottom"><!-- BOTTOM-->
       <!-- Remember to change the link href, the latest date, and the image source. -->
-      <a class="fade" href="#latestimage" onclick="window.open('./i/entities/unknownalienbugs.jpg','Images','status=2');">Latest Mirrored Image: July 13th, 2005</a><br />
+      <ul>
+      <li><a class="fade" href="./i/entities/unknownalienbugs.jpg">Latest Mirrored Image: July 13th, 2005</a></li>
       <!-- Image hidden for now <img src="./i/entities/unknownalienbugs.jpg" alt="mirrored image" /><br /> -->
-      <a class="fade" href="#mirrorimagedir" onclick="window.open('./i/entities/','Images','status=2');">Mirror /Dir</a><br />
-      <a href="http://www.shorturl.com/ref/in.cgi?nxs.vze.com" target="_blank" class="fade">ShortURL.com</a><br />
-      <a href="http://validator.w3.org/check?uri=referer" target="_blank" class="fade">Check Page Code</a><br />
+      <li><a class="fade" href="./i/entities/">Mirror /Dir</a></li>
+      <li><a href="http://www.shorturl.com/ref/in.cgi?nxs.vze.com" target="_blank" class="fade">ShortURL.com</a></li>
+      <li><a href="http://validator.w3.org/check?uri=referer" target="_blank" class="fade">Check Page Code</a></li>
+      <ul>
       
       <div>
       	<a href='http://github.com/tchalvak'>github.com/tchalvak</a>
       </div>
     <ul id="repos"> <!-- GITHUB REPOS -->
       <li><h3>repositories</h3></li>
+      <li id='latest-commit-title'>NW latest commit:</li>
+      <li id='latest-commit'></li>
     </ul>
     </div>
     
@@ -296,4 +341,3 @@
     </div> <!-- End of tchalvak.github.com type content -->
   </body>
 </html>
-
